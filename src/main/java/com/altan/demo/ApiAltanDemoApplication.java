6547ca240499a79e709cc0ba2b9553fdbe9fdf1f
Victor Montes Urbina <vmontesu@usersad.everis.int>
2017-12-20 10:42:25 -0600

    Initial commit

---
+++ src/main/java/com/altan/demo/ApiAltanDemoApplication.java
@@ -0,0 +1,12 @@
+package com.altan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAltanDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAltanDemoApplication.class, args);
+	}
+}
